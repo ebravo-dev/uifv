c939f2cea29eb26bb99c02a2e71dfd5dc5956b40
Eder <a2133330155@alumnos.uat.edu.mx>
2021-06-28 19:09:07 -0500

    csv

--- lib/pages/testPage.dart
+++ lib/pages/testPage.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:csv/csv.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:share/share.dart';
 import 'package:intl/intl.dart';
 import 'package:ui2/widgets/log_activity_card.dart';
@@ -18,7 +22,7 @@ class TestPage extends StatelessWidget {
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
           children: List.generate(
-            3,
+            1,
             (index) => LogActivityCard(
               dateTime: DateTime.now().toIso8601String(),
               urlImage:
@@ -28,6 +32,29 @@ class TestPage extends StatelessWidget {
             ),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        heroTag: null,
+        child: Icon(Icons.share),
+        backgroundColor: Color(0xff03568c),
+        onPressed: () async {
+          List encabezado = ['Producto', 'Actividad', 'Fecha'];
+          String csv = const ListToCsvConverter().convert([
+            encabezado,
+            ['Girasol', 'Solear', '29 Jun 2021, 13:03 PM'],
+            ['Fresa', 'Regar', '28 Jun 2021, 16:03 PM'],
+            ['Fresa', 'Regar', '27 Jun 2021, 17:03 PM'],
+            ['Girasol', 'Regar', '26 Jun 2021, 16:03 PM'],
+            ['Fresa', 'Sembrar', '25 Jun 2021, 15:03 PM'],
+            ['Girasol', 'Sembrar', '24 Jun 2021, 16:03 PM'],
+          ]);
+          final directory = await getApplicationDocumentsDirectory();
+          final pathOfTheFileToWrite = directory.path + "/myCsvFile.csv";
+          File file = File(pathOfTheFileToWrite);
+          await file.writeAsString(csv);
+          await Share.shareFiles(['${directory.path}/myCsvFile.csv'],
+              text: 'Great picture');
+        },
       ),
     );
   }
